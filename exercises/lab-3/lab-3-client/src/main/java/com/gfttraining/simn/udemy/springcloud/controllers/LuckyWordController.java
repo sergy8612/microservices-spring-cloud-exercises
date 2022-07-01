b715110a4a3157c8195532a09a0c96b383728cb9
Sergio Mena Doce <sergy8612@gmail.com>
2022-07-01 11:59:53 +0200

    complete Lab 8

--- exercises/lab-3/lab-3-client/src/main/java/com/gfttraining/simn/udemy/springcloud/controllers/LuckyWordController.java
+++ exercises/lab-3/lab-3-client/src/main/java/com/gfttraining/simn/udemy/springcloud/controllers/LuckyWordController.java
@@ -1,13 +1,33 @@
 package com.gfttraining.simn.udemy.springcloud.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties(prefix = "word-config")
 public class LuckyWordController {
 
-    @Value("${lucky-word}") String luckyWord;
+    String luckyWord;
+
+    public String getLuckyWord() {
+        return luckyWord;
+    }
+
+    public void setLuckyWord(String luckyWord) {
+        this.luckyWord = luckyWord;
+    }
+
+    public String getPreamble() {
+        return preamble;
+    }
+
+    public void setPreamble(String preamble) {
+        this.preamble = preamble;
+    }
+
+    String preamble;
 
     @GetMapping("/lucky-word")
     public String showLuckyWord() {
